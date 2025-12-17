12e2ff36018a11f3a88c705887d511963241bfa9
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2025-12-17 09:35:46 -0500

    Harmonize spellings of "modules native" in code and display
        native modules => modules native
        module native => modules native

--- tools/generate_vcpkg_package_list.py
+++ tools/generate_vcpkg_package_list.py
@@ -30,7 +30,7 @@ def read_vcpkg_json(json_path):
             "current_min_cpp_version": "Unknown",
             "tracking_issue": "", # Issue that tracks the progress
             "modules_support_date": "",  # ISO 8601 date (YYYY-MM-DD), default empty
-            "module_native": "", # Whether if the modules are used as a wrapper or natively
+            "modules_native": "", # Whether if the modules are used as a wrapper or natively
         }
     except Exception as e:
         print(colored(f"Error reading {json_path}: {str(e)}", "red"))
